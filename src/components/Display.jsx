c532437f43034f2ae637d09973b8a11e47d8423b
wande <yewandeadeyemi84@gmail.com>
2023-11-11 15:03:28 +0100

    practiced routing

--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,13 +1,25 @@
+import React from 'react';
+import { BrowserRouter,Routes, Route, Link } from 'react-router-dom';
 import Navbar from './Navigation/NavBar';
 import Footer from './Footer/Footer';
-
+import Home from '../Pages/Home';
+import About from '../Pages/About';
+import Contact from '../Pages/Contact';
 
 
 const Display = () => {
   return (
     <div>
-          <Navbar/>
-  <Footer/>
+  <BrowserRouter>
+     <Navbar />
+      <Routes>
+        <Route index element={<Home />}/>
+        <Route path="about" element={<About />}/>
+        <Route path="contact" element={<Contact />}/>
+     </Routes>
+     <Footer />
+ </BrowserRouter>
+
     </div>
 
   )
